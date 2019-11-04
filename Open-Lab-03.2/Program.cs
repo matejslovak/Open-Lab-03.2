a7c66caf15ca87b70016560f2c012cd9bc6e1faa
matejslovak <joggurtik1@gmail.com>
2019-11-04 09:47:16 +0100

    Open_Lab_03._02

---
+++ Open-Lab-03.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Open_Lab_03._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] array = new string[,] { { "one", "two", "three" }, { "melon", "apple", "strawberry" } };
+            for (int row = 0; row < 2; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    Console.Write($"{array[row, col]}");
+                    Console.WriteLine(" ");
+                }
+                Console.WriteLine(" ");
+            }
+
+        }
+    }
+}
